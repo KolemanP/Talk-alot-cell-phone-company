6aa74b74d8ed7568445cef6295319e77a21aaad0
CAMARILLO <ec0855797@otc.edu>
2018-11-01 13:02:41 -0500

    upload

--- TalkALot/src/talkalot/OutgoingPhoneCall.java
+++ TalkALot/src/talkalot/OutgoingPhoneCall.java
@@ -1,14 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package talkalot;
 
-/**
- *
- * @author ec0855797
- */
-public class OutgoingPhoneCall {
+
+public class OutgoingPhoneCall extends PhoneCall
+{
+    //Variables
+    double callTime, callPrice;
     
+    //Class Constructor
+    public OutgoingPhoneCall(String phoneNumber, double time)
+    {
+        super(phoneNumber);
+        callTime = time;
+    }
+    
+    @Override
+    public double getCallPrice(double time)
+    {
+        double price;
+        price = time * .04;
+        return price;
+    }
+    
+    @Override 
+    public String getPhoneNum()
+    {
+        return this.phoneNumber;
+    }
+    
+    @Override 
+    public void getInfo()
+    {
+        System.out.println("Phone number: " + this.getPhoneNum());
+        System.out.println("\nPrice per minute: 0.04");
+        System.out.println("\nNumber of minutes: " + this.callTime);
+        System.out.println("Total Price: " + getCallPrice(this.callTime));
+    }
+
 }
