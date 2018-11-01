d2dc9fa1ba243147bace48094e569a1690fbd3ef
SLAYDEN <cs0888866@otc.edu>
2018-11-01 12:17:15 -0500

    updates

--- TalkALot/src/talkalot/PhoneCall.java
+++ TalkALot/src/talkalot/PhoneCall.java
@@ -10,5 +10,25 @@ package talkalot;
  * @author cs0888866
  */
 public abstract class PhoneCall {
+    String phoneNumber;
+    double callPrice;
+    abstract double getcallPrice(){};
+};
+    
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    
+   
+
+    public void setCallPrice(double callPrice) {
+        this.callPrice = callPrice;
+    }    
+    
+    PhoneCall(String pNum){
+        this.callPrice = 0.0;
+    }
+    
     
 }
