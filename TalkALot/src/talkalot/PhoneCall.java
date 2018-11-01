8bb2ec60fa85f6aa230d8b48c812886daf21e507
SLAYDEN <cs0888866@otc.edu>
2018-11-01 12:00:09 -0500

    created PhoneCall

---
+++ TalkALot/src/talkalot/PhoneCall.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package talkalot;
+
+/**
+ *
+ * @author cs0888866
+ */
+public abstract class PhoneCall {
+    
+}
